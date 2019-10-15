f59a99f948431334601efb15c574ac32534a8a50
Gabriel Vande Hei <gfvandehei@gmail.com>
2019-10-15 19:54:14 -0400

    Added some basic handlers for udp messages

--- RotiniServer/src/devicesocketserver/devicesocketserver.ts
+++ RotiniServer/src/devicesocketserver/devicesocketserver.ts
@@ -15,6 +15,28 @@ export class DeviceSocketServer{
         this.serverSocket.on("listening", () => {
             const sockaddr = this.serverSocket.address() as AddressInfo;
             console.log(`Device server listening on ${sockaddr.address}: ${sockaddr.port}`);
-        })
+        });
+        this.serverSocket.on('error', (err) => this.onError(err));
+        this.serverSocket.on('message', (message, addrinfo) => this.onMessage(message, addrinfo));
+    }
+
+    private onError(error: Error){
+        console.log(`There was a server error:\n ${error.stack}`);
+        this.serverSocket.close();
+    }
+
+    private onMessage(message: Buffer, recvAddr: AddressInfo){
+        console.log(`Received message ${message} from ${recvAddr.address}:${recvAddr.port}`);
+        const opcodeChunk = message.slice(0,2);
+        const opcode = opcodeChunk.readUInt16BE(0);
+        console.log(`received opcode ${opcode}`);
+    }
+
+    public getConnectedDeviceNames(){
+        
+    }
+
+    public getDevice(deviceName: String){
+
     }
 }
